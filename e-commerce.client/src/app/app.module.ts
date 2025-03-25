542294a9dc3394020881a62d6a6dca9efe255c3d
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-03-25 16:09:51 +0300

    E-commerce

--- e-commerce.client/src/app/app.module.ts
+++ e-commerce.client/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './Farah/about/about.component';
+import { CartComponent } from './Firas/cart/cart.component';
+import { ContactComponent } from './Habeeb/contact/contact.component';
+import { HomeComponent } from './Hazem/home/home.component';
+import { ProductDetailsComponent } from './Jana/product-details/product-details.component';
+import { ProfileComponent } from './Sally/profile/profile.component';
+import { ShopComponent } from './Sondos/shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CartComponent,
+    ContactComponent,
+    HomeComponent,
+    ProductDetailsComponent,
+    ProfileComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,

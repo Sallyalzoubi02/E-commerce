65d4ebe0fb5b3b7085b88c578496e94890471cca
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-03-26 14:59:51 +0300

    commit for dashboard

--- e-commerce.client/src/app/app-routing.module.ts
+++ e-commerce.client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Hazem/home/home.component';
+import { ProfileComponent } from './Sally/profile/profile.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', pathMatch: 'full', redirectTo: '/home' }
 ];
 
